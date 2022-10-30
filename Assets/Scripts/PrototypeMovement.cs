4929e476e0b03d663bd5705a7220f444a01678f2
Techmarine117 <keegan.strickler@gmail.com>
2022-10-31 01:14:36 +0400

    prototyped a movement script
    prototyped a movement script the prototypes movement script runs off of the old unity input system I also began to experiment with the new input system in the hopes of making a more streamlined character controller in the future.

---
+++ Assets/Scripts/PrototypeMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public float jumpdist;
+
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+    public bool canSprint;
+
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        canSprint = true;
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // edited
+        if (!isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canSprint)
+        {
+            speed = 6f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 4f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            controller.height = 0.5f;
+            speed = 5f;
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            controller.height = 2.0f;
+            speed = 12f;
+        }
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = jumpdist;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
