3036dcdf0712ed20a2b62d5ae9ee2d4f7b93e28f
Techmarine117 <keegan.strickler@gmail.com>
2022-11-07 21:46:57 +0400

    Add the ability to pick up objects
    the player can now pick up objects are marked with the tag pickup I've also went ahead and important, more features for text mesh Pro. it will come in handy in the future

---
+++ Assets/Scripts/PlayerRaycast.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject popUp;
+    private GameObject pickUpobj;
+    public float Raylength;
+    private bool isPickup;
+    public Transform pickupPlacholder;
+    public PrototypeMovement pm;
+    void Update()
+    {
+        // to stop any further action that has to do with raycasting while an object is picked up
+        if (isPickup == true && pickUpobj)
+        {
+            pm.canSprint = false;         
+            pickUpobj.transform.rotation = transform.rotation;
+            pickUpobj.transform.position = pickupPlacholder.position;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                pickUpobj.AddComponent<Rigidbody>();
+                isPickup = false;
+                pickUpobj = null;
+                pm.canSprint = true;
+                
+
+            }
+            return;
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, Raylength))
+        {
+            if (hit.collider.tag == "interactible")
+            {
+                //do action
+               // hit.collider.GetComponent<interactibleObj>().Action();
+                popUp.SetActive(true);
+                Debug.Log("hit");
+
+
+
+            }
+
+
+            if (hit.collider.tag == "pickUp")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    isPickup = true;
+                    pickUpobj = hit.collider.gameObject;
+                    Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+                    Destroy(rb);
+
+                }
+            }
+
+           
+        }
+
+    }
+}
