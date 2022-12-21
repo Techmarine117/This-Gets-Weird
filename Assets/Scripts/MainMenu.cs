1545ef4fa0338a27dacee55be299d25e81a9b8a6
Techmarine117 <keegan.strickler@gmail.com>
2022-12-21 22:40:15 +0400

    Setup Options for audio

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    
     public string NewGame;
     private string LevelToLoad;
     public DataManager dataManager;
     public GameObject NoSaveObj = null;
+
+    [Header("Volume Settings")]
+    public TMP_Text VolumeTextValue = null;
+    public Slider VolumeSlider = null;
+    public GameObject ConfirmationPrompt = null;
+    public float DefaultVolume = 5.0f;
+
 
     private void Awake()
     {
@@ -40,5 +49,37 @@ public class MainMenu : MonoBehaviour
         {
             NoSaveObj.SetActive(true);
         }
+    }
+
+    public void SetVolume(float Volume)
+    {
+        AudioListener.volume = Volume;
+
+        VolumeTextValue.text = Volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationText());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = DefaultVolume;
+            VolumeSlider.value = DefaultVolume;
+            VolumeTextValue.text = DefaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationText()
+    {
+        ConfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(5);
+
+        ConfirmationPrompt.SetActive(false);
     }
 }
