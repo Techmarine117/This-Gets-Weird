34c0cf9a15fed9eadada180585ae970a3a46ecc0
Techmarine117 <keegan.strickler@gmail.com>
2022-12-21 18:59:04 +0400

    Updated the MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public string NewGame;
+    private string LevelToLoad;
+    public DataManager dataManager;
+    public GameObject NoSaveObj = null;
+
+    private void Awake()
+    {
+        dataManager = GameObject.FindObjectOfType<DataManager>();
+    }
     public void quitGame()
     {
         Debug.Log("quiting game");
@@ -12,9 +22,23 @@ public class MainMenu : MonoBehaviour
 
     }
 
-    public void loadlevel()
+    public void loadNewGame()
     {
         Debug.Log("level Loading");
-        SceneManager.LoadScene("Blockout");
+        SceneManager.LoadScene(NewGame);
+    }
+
+    public void LoadLevel()
+    {
+        dataManager.LoadGame();
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            LevelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(LevelToLoad);
+        }
+        else
+        {
+            NoSaveObj.SetActive(true);
+        }
     }
 }
