394efe9315bc6084a806691f1c9a14ebc6a8638f
Jeff Taylor <taylorjeff833@gmail.com>
2023-02-20 10:51:36 +0400

    Got the switch working with new prefab and now using the dialogue system trigger instead of the old script

--- Assets/_Scripts/PuzzlesScripts/SwitchManager.cs
+++ Assets/_Scripts/PuzzlesScripts/SwitchManager.cs
@@ -8,43 +8,52 @@ public class SwitchManager : MonoBehaviour
 
     public bool isSwitchOn = false;
 
-    public float castLength = 5f;
+    //public float castLength = 5f;
 
-    public GameObject gameCam;
+    //public GameObject gameCam;
 
-    public GameObject switchCol;
+    //public GameObject switchCol;
     private void Update()
     {
-        RaycastHit hit;
+        //RaycastHit hit;
 
-        if (Physics.Raycast(gameCam.transform.position, gameCam.transform.forward, out hit, castLength))
-        {
-            if (hit.collider.gameObject == switchCol)
-            {
-                if (Input.GetKeyDown(KeyCode.E) && isSwitchOn == false)
-                {
-                    SwitchOn();
-                }
-                else if (Input.GetKeyDown(KeyCode.E) && isSwitchOn == true)
-                {
-                    SwitchOff();
-                }
-            }
-        }
+        //if (Physics.Raycast(gameCam.transform.position, gameCam.transform.forward, out hit, castLength))
+        //{
+        //    if (hit.collider.gameObject == switchCol)
+        //    {
+        //        if (Input.GetKeyDown(KeyCode.E) && isSwitchOn == false)
+        //        {
+        //            SwitchOn();
+        //        }
+        //        else if (Input.GetKeyDown(KeyCode.E) && isSwitchOn == true)
+        //        {
+        //            SwitchOff();
+        //        }
+        //    }
+        //}
 
         
     }
-    public void SwitchOn()
+    public void SwitchControl()
     {
-        isSwitchOn = true;
-        switchAnim.SetTrigger("On");
+        if (!isSwitchOn)
+        {
+            StartCoroutine(SwitchOn());
+        }
+        else StartCoroutine(SwitchOff());
     }
 
-    public void SwitchOff()
+    public IEnumerator SwitchOff()
     {
-        isSwitchOn = false;
         switchAnim.SetTrigger("Off");
+        yield return new WaitForSeconds(0.35f);
+        isSwitchOn = false;
+
     }
-
-
+    public IEnumerator SwitchOn()
+    {
+        switchAnim.SetTrigger("On");
+        yield return new WaitForSeconds(0.35f);
+        isSwitchOn = true;
+    }
 }
