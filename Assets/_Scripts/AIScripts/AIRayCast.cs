5aa65eb6978ce5d13e11235c43fa88e291e570d7
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-03-14 11:52:43 +0400

    Fixed AI bug

--- Assets/_Scripts/AIScripts/AIRayCast.cs
+++ Assets/_Scripts/AIScripts/AIRayCast.cs
@@ -1,85 +1,53 @@
-using JetBrains.Annotations;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class AIRayCast : MonoBehaviour
 {
-    
-    
     public float ViewRadius;
-    [Range(0,360)]
-    public float ViewAngle;
+    [Range(0, 360)] public float ViewAngle;
     public bool PlayerHit;
-    public LayerMask targetMask;
     public LayerMask ObstacleMask;
     public GameObject PlayerReference;
 
     private void Start()
     {
         PlayerReference = GameObject.FindGameObjectWithTag("Player");
-        StartCoroutine(FOVRout());
     }
 
-    private IEnumerator FOVRout()
+    private void Update()
     {
-       
-        WaitForSeconds Wait = new WaitForSeconds(0.2f);
-        while (true)
-        {
-            yield return Wait;
-            FindVisibleTargets();
-        }
+        FindVisibleTargets();
     }
-
-    
 
     public void FindVisibleTargets()
     {
-        Debug.Log("FINDING VISIBLE TARGETS");
-        Collider[] RangeChecks = Physics.OverlapSphere(transform.position, ViewRadius, targetMask);
-        Debug.Log(RangeChecks);
-        if(RangeChecks.Length != 0)
+        PlayerHit = false;
+        var playerIOnRange = IsPlayerInRange();
+        var dir = IsPlayerInFOV(out var playerInFOV);
+        var hasLineOfSight = IsThereAnObjectBetweenPlayerAndMe(dir);
+        if (playerIOnRange && playerInFOV && hasLineOfSight)
         {
-            Debug.Log("Range Check line 46");
-
-            Transform Target = RangeChecks[0].transform;
-            Vector3 DirToTarget = (Target.position - transform.position).normalized;
-
-            if (Vector3.Angle(transform.forward, DirToTarget) < ViewAngle/2 )
-            {
-                Debug.Log("Changing Angle Check");
-
-                float DisToTarget = Vector3.Distance(transform.position, Target.position);
-
-                if(!Physics.Raycast(transform.position, DirToTarget, DisToTarget, ObstacleMask))
-                {
-                    Debug.Log("Player Hit true");
-                    PlayerHit = true;
-                }
-                else 
-                {
-                    Debug.Log("Player Hit False 3");
-                PlayerHit = false; 
-                }      
-            }
-            else
-            {
-                Debug.Log("Player Hit False 2");
-                PlayerHit = false;
-            }
-        }
-        else if (PlayerHit) {
-            Debug.Log("Player Hit False 1");
-            PlayerHit = false;
+            PlayerHit = true;
         }
     }
 
-    
-    
+    private bool IsThereAnObjectBetweenPlayerAndMe(Vector3 dir)
+    {
+        bool hasLineOfSight = !Physics.Raycast(transform.position, dir, ViewRadius, ObstacleMask);
+        return hasLineOfSight;
+    }
 
+    private Vector3 IsPlayerInFOV(out bool playerInFOV)
+    {
+        Vector3 dir = (PlayerReference.transform.position - transform.position).normalized;
+        playerInFOV = Vector3.Angle(transform.forward, dir) < ViewAngle / 2;
+        return dir;
+    }
 
-    
+    private bool IsPlayerInRange()
+    {
+        float distance = Vector3.Distance(transform.position, PlayerReference.transform.position);
+        bool playerIOnRange = distance < ViewRadius;
+        return playerIOnRange;
+    }
 }
