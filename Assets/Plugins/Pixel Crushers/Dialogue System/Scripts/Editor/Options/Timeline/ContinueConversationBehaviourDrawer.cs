f4c2009f63da173f1f044493793cc54a22329b4f
Jeff Taylor <taylorjeff833@gmail.com>
2023-03-15 21:53:40 +0400

    put in timline support for dialogue system

--- Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Editor/Options/Timeline/ContinueConversationBehaviourDrawer.cs
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Editor/Options/Timeline/ContinueConversationBehaviourDrawer.cs
@@ -1,4 +1,5 @@
-﻿#if USE_TIMELINE
+// Recompile at 3/15/2023 9:09:16 PM
+#if USE_TIMELINE
 #if UNITY_2017_1_OR_NEWER
 using UnityEngine;
 using UnityEditor;
--- Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Editor/Options/Timeline/PlaySequenceBehaviourDrawer.cs
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Editor/Options/Timeline/PlaySequenceBehaviourDrawer.cs
@@ -1,3 +1,4 @@
+// Recompile at 3/15/2023 9:09:16 PM
 #if USE_TIMELINE
 #if UNITY_2017_1_OR_NEWER
 using UnityEditor;
