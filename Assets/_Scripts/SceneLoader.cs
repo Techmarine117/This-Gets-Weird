fa3621715f16137601b6e642e94aff7bd4cc1296
Jeff Taylor <taylorjeff833@gmail.com>
2023-03-22 13:07:42 +0400

    Build 1

--- Assets/_Scripts/SceneLoader.cs
+++ Assets/_Scripts/SceneLoader.cs
@@ -5,11 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    [SerializeField] GameObject pauseMenu;
+    //[SerializeField] GameObject pauseMenu;
     static bool gameIsPaused;
     public void LoadScene(string sceneName)
     {
-        pauseMenu.SetActive(false);
+        //pauseMenu.SetActive(false);
         Time.timeScale = 1;
         SceneManager.LoadScene(sceneName);
     }
@@ -19,35 +19,35 @@ public class SceneLoader : MonoBehaviour
         Application.Quit();
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (gameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Escape))
+    //    {
+    //        if (gameIsPaused)
+    //        {
+    //            Resume();
+    //        }
+    //        else
+    //        {
+    //            Pause();
+    //        }
+    //    }
         
-    }
-    void Pause()
-    {
-        gameIsPaused = true;
-        pauseMenu.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        Time.timeScale = 0;
-    }
-    void Resume()
-    {
-        gameIsPaused = false;
-        pauseMenu.SetActive(false);
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        Time.timeScale = 1;
-    }
+    //}
+    //void Pause()
+    //{
+    //    gameIsPaused = true;
+    //    pauseMenu.SetActive(true);
+    //    Cursor.lockState = CursorLockMode.None;
+    //    Cursor.visible = true;
+    //    Time.timeScale = 0;
+    //}
+    //void Resume()
+    //{
+    //    gameIsPaused = false;
+    //    pauseMenu.SetActive(false);
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //    Cursor.visible = false;
+    //    Time.timeScale = 1;
+    //}
 }
