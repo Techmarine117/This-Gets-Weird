ee7e742d6cfd13dfcf8e389c6e109b404c6c52f2
Techmarine117 <keegan.strickler@gmail.com>
2023-03-23 17:00:29 +0400

    Save fix

---
+++ Assets/_Scripts/MainMenuScripts/InputChangeScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputChangeScript
+{
+    public static KeyCode jumpKey;
+    public static KeyCode sprintKey;
+    public static KeyCode crouchKey;
+
+}
