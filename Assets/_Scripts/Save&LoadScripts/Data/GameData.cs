44edb365307725848120d0ea1f19123c7412681d
Techmarine117 <keegan.strickler@gmail.com>
2023-04-17 21:15:28 +0400

    Updated the  Save code
    still have some problems to sort out with it

--- Assets/_Scripts/Save&LoadScripts/Data/GameData.cs
+++ Assets/_Scripts/Save&LoadScripts/Data/GameData.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 [System.Serializable]
 public class GameData 
 {
-    public int Test;
+    
     public Vector3 PlayerPosition, AIPosition;
+    public bool Mask1,Mask2,Mask3;
     
 
     //the value we defined in this constructor will be the default values
     public GameData()
     {
-        this.Test = 0;
+        
         PlayerPosition = new Vector3(80,51,9);
         AIPosition = Vector3.zero;
+        Mask1 = false;
+        Mask2 = false;
+        Mask3 = false;
 
     }
 }
